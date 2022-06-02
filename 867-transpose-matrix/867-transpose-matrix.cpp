a747de476b02a61925a5b9eb049539707478d4a5
Yash Mina <96470436+yashmina01@users.noreply.github.com>
2022-06-03 04:57:36 +0530

    Time: 15 ms (65.95%), Space: 10.7 MB (42.65%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -5,7 +5,7 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         
-        vector<vector<int> > newMat(m,vector<int> (n));
+        vector<vector<int> > newMat(m,vector<int> (n,0));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
