b3c47fecf581cf46443dcb3a701fbd8e379334f3
Yash Mina <96470436+yashmina01@users.noreply.github.com>
2022-06-08 14:07:10 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -5,43 +5,52 @@ using namespace std;
  // } Driver Code Ends
 vector<int> find(int arr[], int n , int x )
 {
-  vector<int>ans;
-   int first = -1;
-   int low=0,high=n-1;
-   
-   while(low <= high){
-       int mid = low + (high-low)/2;
-       
-       if(arr[mid] == x){
-           first = mid;
-           high = mid - 1;
-       }
-       else if(x < arr[mid]){
-           high=mid-1;
-       }
-       else
-           low = mid + 1;
-   }
-   ans.push_back(first);
-   
-   int last = -1;
-   int start=0,end = n-1;
-   
-   while(start <= end){
-       
-       int mid=start + (end-start)/2;
-       if(arr[mid] == x){
-           last = mid;
-           start = mid + 1;
-       }
-       else if(x > arr[mid]){
-           start=mid+1;
-       }
-       else
-           end = mid - 1;
-   }
-   ans.push_back(last);
-   return ans;
+    vector<int> ans;
+    
+  int start = 0,end = n-1;
+  int first = -1;
+  int mid = start+(end-start)/2;
+  
+  while(start<=end){
+      
+      mid = start+(end-start)/2;
+      
+      if(arr[mid] == x){
+          first = mid;
+      end = mid-1;
+      }
+      else if(x > arr[mid])
+      start = mid+1;
+      
+      else end = mid-1;
+  }
+  if(first == -1)
+      ans.push_back(-1);
+  else
+      ans.push_back(first);
+  
+  int low = 0,high=n-1;
+  int last = -1;
+  mid = low+(high-low)/2;
+  
+  while(low<=high){
+      
+      mid = low+(high-low)/2;
+      
+      if(arr[mid] == x){
+      last = mid;
+      low = mid + 1;
+      }
+      else if(x > arr[mid])
+      low = mid+1;
+      
+      else high = mid-1;
+  }
+  if(last == -1)
+      ans.push_back(-1);
+  else
+      ans.push_back(last);
+  return ans;
 }
 
 // { Driver Code Starts.
