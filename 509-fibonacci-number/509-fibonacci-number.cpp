8380a8337ddb07a4ad886b16f1380dce2a6786c3
Yash Mina <96470436+yashmina01@users.noreply.github.com>
2022-06-12 02:07:50 +0530

    Time: 4 ms (51.67%), Space: 6 MB (41.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     int fib(int n) {
         
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
-        
-        return fib(n-1) + fib(n-2);
+    int first = 0,second = 1;
+    int next;
+
+    for(int i=0;i<n;i++){
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    return first;
     }
 };
